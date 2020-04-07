bb0211a5510b298a11aac0b8c3378405a83c7ed8
Brad Corbett <brc9@aber.ac.uk>
2020-04-07 03:27:12 +0100

    Added 'Translation' test demo project.

---
+++ dev/20200331/brc9/translationFX/src/Welsh Vocabulary Tutor/src/main/java/uk/ac/aber/cs22120/group20/App.java
@@ -0,0 +1,56 @@
+package uk.ac.aber.cs22120.group20;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Launch class of the Welsh Vocabulary tutor Application
+ *
+ * @author Brad Corbett [brc9]
+ * @author Henry Dugmore [hjd3]
+ * @author Kain Bryan-Jones [kab74]
+ * @author Luke Wybar [law39]
+ * @author Marcin Jakob [maj83]
+ * @author Oscar Pocock [osp1]
+ * @author Tom Perry [top1]
+ * @author Waylen Watts [ncw]
+ *
+ * @version 0.1
+ */
+public class App extends Application {
+    private static Scene scene;
+
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("translationTest"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+
+        launch();
+
+
+
+    }
+
+
+
+}
