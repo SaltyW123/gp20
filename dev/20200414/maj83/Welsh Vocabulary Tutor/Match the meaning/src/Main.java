4c1eccb45879503de533df1b3b5938e6cb5aeff3
maj83 <maj83@aber.ac.uk>
2020-04-21 12:49:27 +0100

    Added loading from dictionary.

--- dev/20200414/maj83/Welsh Vocabulary Tutor/Match the meaning/src/Main.java
+++ dev/20200414/maj83/Welsh Vocabulary Tutor/Match the meaning/src/Main.java
@@ -4,12 +4,20 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.util.LinkedList;
+
 
 public class Main extends Application{
 
+    public static LinkedList<DictionaryEntry> dictionary = new LinkedList();
+
     public static void main(String[] args) {
-
-
+        dictionary.add(new DictionaryEntry("abbey", "abaty", "nm"));
+        dictionary.add(new DictionaryEntry("believe", "credu", "verb"));
+        dictionary.add(new DictionaryEntry("concert", "cyngerdd", "nm"));
+        dictionary.add(new DictionaryEntry("disease", "clefyd", "nm"));
+        dictionary.add(new DictionaryEntry("extremely", "dros ben", "other"));
+        dictionary.add(new DictionaryEntry("flu", "ffliw", "nm"));
         launch(args);
     }
 
