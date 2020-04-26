14e1e52f250659ff665fa751a2ed5c177a8bb041
osp1 <osp1@aber.ac.uk>
2020-04-26 18:26:49 +0100

    Added fully functional Dictionary & Practice pages
    Dictionary and Practice pages both are 100% functional.
    The only a few changes need to still be made:
    - Commenting
    - Alpha sort icons needs to change on click
    - Highlighted words need to have white text (CSS)
    - Headers need to be removed (CSS)

---
+++ dev/20200421/osp1/Welsh Vocabulary Tutor/src/main/java/uk/ac/aber/cs22120/group20/DictionaryController.java
@@ -0,0 +1,194 @@
+/**
+ * @(#) DictionaryController.java 0,1 2020/04/07
+ * <p>
+ * Copyright (c) 2020 Aberystwyth University.
+ * All rights reserved.
+ */
+package uk.ac.aber.cs22120.group20;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import uk.ac.aber.cs22120.group20.json.DictionaryEntry;
+
+/**
+ * A class that handles the keyboard and mouse input and interaction for the 'Dictionary Page' which is
+ * defined by 'dictionary.fxml'
+ *
+ * @author Brad Corbett [brc9]
+ * @author Henry Dugmore [hjd3]
+ * @author Kain Bryan-Jones [kab74]
+ * @author Luke Wybar [law39]
+ * @author Marcin Jakob [maj83]
+ * @author Oscar Pocock [osp1]
+ * @author Tom Perry [top1]
+ * @author Waylen Watts [ncw]
+ * @version 0.1 Initial development.
+ * @see DictionaryEntry
+ * @see Application
+ */
+public class DictionaryController implements Initializable {
+    public static Stage primaryStage = null;
+
+    @FXML
+    private TextField searchBox;
+    @FXML
+    private TableView<DictionaryEntry> table;
+    @FXML
+    private TableColumn<DictionaryEntry, String> english = new TableColumn<>();
+    @FXML
+    private TableColumn<DictionaryEntry, String> welsh = new TableColumn<>();
+
+    public ObservableList<DictionaryEntry> list = FXCollections.observableArrayList();
+
+    @FXML
+    private void switchLangSort() {
+        if (table.getSortOrder().contains(english)) {
+            table.getSortOrder().clear();
+            table.getSortOrder().add(welsh);
+        } else if (table.getSortOrder().contains(welsh)) {
+            table.getSortOrder().clear();
+            table.getSortOrder().add(english);
+        }
+        table.sort();
+    }
+
+    @FXML
+    private void switchAlphaSort() {
+        if (table.getSortOrder().contains(english)) {
+            if (english.getSortType().equals(TableColumn.SortType.ASCENDING)) {
+                english.setSortType(TableColumn.SortType.DESCENDING);
+            } else {
+                english.setSortType(TableColumn.SortType.ASCENDING);
+            }
+        } else if (table.getSortOrder().contains(welsh)) {
+            if (welsh.getSortType().equals(TableColumn.SortType.ASCENDING)) {
+                welsh.setSortType(TableColumn.SortType.DESCENDING);
+            } else {
+                welsh.setSortType(TableColumn.SortType.ASCENDING);
+            }
+        }
+    }
+
+    /**
+     * Switches to the primary scene.
+     *
+     * @throws IOException
+     */
+    @FXML
+    private void switchToPracticeList() throws IOException {
+        Application.setRoot("practicelist");
+    }
+
+    /**
+     * Initializes the  table of dictionary entries.
+     * <p>
+     * An observable list of DictionaryEntries is loaded from the Application class into a local instance of ObservableList.
+     * It also sets up Lambda expressions related to live searching functionality and the display of DictionaryEntries.
+     *
+     * @param url
+     * @param resourceBundle
+     * @see Application
+     * @see DictionaryEntry
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        list.addAll(Application.dictionary);
+
+        table.setRowFactory(tv -> {
+                    TableRow<DictionaryEntry> row = new TableRow<DictionaryEntry>() {
+                        @Override
+                        protected void updateItem(DictionaryEntry dictionaryEntry, boolean b) {
+                            super.updateItem(dictionaryEntry, b);
+                            if (!isEmpty()) {
+                                if (dictionaryEntry.isPracticeWord()) {
+                                    setStyle("-fx-background-color: gray;");
+                                } else {
+                                    setStyle(" ");
+                                }
+                            }
+                        }
+                    };
+                    row.setOnMouseClicked(mouseEvent -> {
+                        if (mouseEvent.getClickCount() == 1 && (!row.isEmpty())) {
+                            if (row.getItem().isPracticeWord()) {
+                                Application.dictionary.get(list.indexOf(row.getItem())).setPracticeWord(false);
+//                        row.getItem().setPracticeWord(false);
+                            } else if (!row.getItem().isPracticeWord()) {
+                                Application.dictionary.get(list.indexOf(row.getItem())).setPracticeWord(true);
+//                        row.getItem().setPracticeWord(true);
+                            }
+                            table.getSelectionModel().clearSelection();
+                        }
+                    });
+                    return row;
+                }
+        );
+        welsh.setCellValueFactory(dictionaryEntryStringCellDataFeatures -> {
+            if (dictionaryEntryStringCellDataFeatures.getValue().getWordType().equals("nm")) {
+                return new SimpleStringProperty(dictionaryEntryStringCellDataFeatures.getValue().getWelsh() + " {nm}");
+            } else if (dictionaryEntryStringCellDataFeatures.getValue().getWordType().equals("nf")) {
+                return new SimpleStringProperty(dictionaryEntryStringCellDataFeatures.getValue().getWelsh() + " {nf}");
+            } else {
+                return new SimpleStringProperty(dictionaryEntryStringCellDataFeatures.getValue().getWelsh());
+            }
+        });
+
+        english.setCellValueFactory(dictionaryEntryStringCellDataFeatures -> {
+            if (dictionaryEntryStringCellDataFeatures.getValue().getWordType().equals("verb")) {
+                return new SimpleStringProperty("to " + dictionaryEntryStringCellDataFeatures.getValue().getEnglish());
+            } else {
+                return new SimpleStringProperty(dictionaryEntryStringCellDataFeatures.getValue().getEnglish());
+            }
+        });
+
+        FilteredList<DictionaryEntry> filteredList = new FilteredList<>(list, p -> true); // Wrap list in a FilteredList
+
+        searchBox.textProperty().addListener((observable, oldSearchTerm, newSearchTerm) -> {
+            filteredList.setPredicate(dictionaryEntry -> { // returns true on a filter match, false if no match
+                boolean result = false;
+
+                table.refresh(); // This fixes the table highlighting issue
+
+                if (newSearchTerm == null || newSearchTerm.isEmpty()) { // If filter text is empty, display all dictionary entries
+                    result = true;
+                } else {
+                    // need all same case for compare.
+                    final String lowerCaseSearchFilter = newSearchTerm.toLowerCase();
+                    if (dictionaryEntry.getWelsh().toLowerCase().contains(lowerCaseSearchFilter)) {
+                        result = true; // Filter matches Welsh
+                    } else if (dictionaryEntry.getEnglish().toLowerCase().contains(lowerCaseSearchFilter)) {
+                        result = true; // Filter matches English
+//                    } else if (dictionaryEntry.getWordType().toLowerCase().contains(lowerCaseSearchFilter)) {
+//                        result = true; // Filter matches Word Type
+                    } else if (dictionaryEntry.getWordType().equals("verb") && ("to " + dictionaryEntry.getEnglish()).toLowerCase().contains(lowerCaseSearchFilter)) {
+                        result = true; // Filter matches ['to' + a word] or [a word] if word is a verb
+                    }
+                }
+                return result;
+            });
+        });
+
+        SortedList<DictionaryEntry> sortedList = new SortedList<>(filteredList); //Wrap the filtered list in a SortedList
+        sortedList.comparatorProperty().bind(table.comparatorProperty()); //Bind the sorted list comparator to the table comparator
+//        welsh.setCellValueFactory(new PropertyValueFactory<DictionaryEntry, String>("welsh"));
+//        english.setCellValueFactory(new PropertyValueFactory<DictionaryEntry, String>("english"));
+
+        table.setItems(sortedList);
+        table.getSortOrder().add(english);
+    }
+
+}
+
---
+++ dev/20200421/osp1/Welsh Vocabulary Tutor/src/main/java/uk/ac/aber/cs22120/group20/json/DictionaryEntry.java
@@ -0,0 +1,90 @@
+/**
+ * @(#) DictionaryEntry.java 0,1 2020/04/07
+ * <p>
+ * Copyright (c) 2020 Aberystwyth University.
+ * All rights reserved.
+ */
+package uk.ac.aber.cs22120.group20.json;
+
+import uk.ac.aber.cs22120.group20.Application;
+import uk.ac.aber.cs22120.group20.DictionaryController;
+
+/**
+ * A class that demonstrates how a controller works.
+ *
+ * @author Brad Corbett [brc9]
+ * @author Henry Dugmore [hjd3]
+ * @author Kain Bryan-Jones [kab74]
+ * @author Luke Wybar [law39]
+ * @author Marcin Jakob [maj83]
+ * @author Oscar Pocock [osp1]
+ * @author Tom Perry [top1]
+ * @author Waylen Watts [ncw]
+ * @version 0.1 Initial development
+ * @see DictionaryController
+ */
+public class DictionaryEntry {
+    private String english;
+    private String welsh;
+    private String wordType;
+    private Boolean practiceWord;
+
+
+    /**
+     * Creates a new instance of a DictionaryEntry
+     */
+    public DictionaryEntry() {
+        practiceWord = false;
+    }
+
+    /**
+     * Creates new instance of a DictionaryEntry
+     *
+     * @param english      english translation of the word
+     * @param welsh        welsh translation of the word
+     * @param wordType     type of word
+     * @see Application
+     * @see DictionaryController
+     */
+    public DictionaryEntry(String english, String welsh, String wordType) {
+        this.english = english;
+        this.welsh = welsh;
+        this.wordType = wordType;
+    }
+
+    public String getEnglish() {
+        return english;
+    }
+
+    public void setEnglish(String english) {
+        this.english = english;
+    }
+
+    public String getWelsh() {
+        return welsh;
+    }
+
+    public void setWelsh(String welsh) {
+        this.welsh = welsh;
+    }
+
+    public String getWordType() {
+        return wordType;
+    }
+
+    public void setWordType(String wordType) {
+        this.wordType = wordType;
+    }
+
+    public Boolean isPracticeWord() {
+        return practiceWord;
+    }
+
+    public void setPracticeWord(Boolean practiceWord) {
+        this.practiceWord = practiceWord;
+    }
+
+    public boolean equals(DictionaryEntry entry) {
+        return entry.getEnglish().equals(this.getEnglish()) && entry.getWelsh().equals(this.getWelsh()) && entry.getWordType().equals(this.getWordType());
+    }
+}
