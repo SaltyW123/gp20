a073a21b28caba892e38d277d28ca022bbb1c232
maj83 <maj83@aber.ac.uk>
2020-04-28 17:38:16 +0100

    Refactored MatchTheMeaningQuestion to MatchTheMeaningController

--- src/Welsh Vocabulary Tutor/src/main/java/uk/ac/aber/cs22120/group20/selfassessment/MatchTheMeaningQuestion.java
+++ src/Welsh Vocabulary Tutor/src/main/java/uk/ac/aber/cs22120/group20/selfassessment/MatchTheMeaningQuestion.java
@@ -10,7 +10,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import uk.ac.aber.cs22120.group20.javafx.Application;
 import uk.ac.aber.cs22120.group20.json.DictionaryEntry;
 import java.net.URL;
 import java.util.*;
@@ -26,145 +25,150 @@ import java.util.*;
  * @author Oscar Pocock [osp1]
  * @author Tom Perry [top1]
  * @author Waylen Watts [ncw]
- * @version 0.1 Initial development
+ * @version 0.4 Initial development
  * @see uk.ac.aber.cs22120.group20.javafx.Application
  */
 
 
-public class MatchTheMeaningQuestion extends Question implements Initializable{
+public class MatchTheMeaningController extends Question implements Initializable{
 
 
-    public static ArrayList<DictionaryEntry> setOfQuestions=new ArrayList<>();
-    private ArrayList<Integer> orderList = new ArrayList<>(Arrays.asList(0,1,2,3));
-    private boolean isEnglish;
+   private ArrayList<DictionaryEntry> setOfQuestions=new ArrayList<>();
+   private ArrayList<Integer> orderList = new ArrayList<>(Arrays.asList(0,1,2,3));
+   private boolean isEnglish;
 
 
-    @FXML
-    private ComboBox<String> word1;
+   @FXML
+   private ComboBox<String> word1;
 
-    @FXML
-    private ComboBox<String> word2;
+   @FXML
+   private ComboBox<String> word2;
 
-    @FXML
-    private ComboBox<String> word3;
+   @FXML
+   private ComboBox<String> word3;
 
-    @FXML
-    private ComboBox<String> word4;
+   @FXML
+   private ComboBox<String> word4;
 
-    @FXML
-    private Label LeftWord1;
+   @FXML
+   private Label LeftWord1;
 
-    @FXML
-    private Label LeftWord2;
+   @FXML
+   private Label LeftWord2;
 
-    @FXML
-    private Label LeftWord3;
+   @FXML
+   private Label LeftWord3;
 
-    @FXML
-    private Label LeftWord4;
+   @FXML
+   private Label LeftWord4;
 
-    @FXML
-    private Label RightWord1;
+   @FXML
+   private Label RightWord1;
 
-    @FXML
-    private Label RightWord2;
+   @FXML
+   private Label RightWord2;
 
-    @FXML
-    private Label RightWord3;
+   @FXML
+   private Label RightWord3;
 
-    @FXML
-    private Label RightWord4;
+   @FXML
+   private Label RightWord4;
 
-    @FXML
-    private Label CorrectAnswer;
+   @FXML
+   private Label CorrectAnswer;
 
-    @FXML
-    private Label WrongAnswer;
-
-
-    /**
-     * Pick randomly dictionary entry and add it to question list where are stored questions for this test.
-     */
-    private void getQuestions(){
-        AssessmentGenerator.generateSixMeanings(Application.dictionary);
-    }
-
-    /**
-     * Set chosen words from dictionary on the scene.
-     *
-     * @param questions list of dictionary entries chosen for this test.
-     * @param orderList list of integers to change way of displaying welsh words.
-     */
+   @FXML
+   private Label WrongAnswer;
 
 
-    private void setWords(ArrayList<DictionaryEntry> questions, ArrayList<Integer> orderList){
+   /**
+    * Pick randomly dictionary entry and add it to question list where are stored questions for this test.
+    */
+   private void getQuestions(){
 
-        if(isEnglish){
-            LeftWord1.setText(questions.get(0).getEnglish());
-            LeftWord2.setText(questions.get(1).getEnglish());
-            LeftWord3.setText(questions.get(2).getEnglish());
-            LeftWord4.setText(questions.get(3).getEnglish());
+      setOfQuestions.addAll(AssessmentGenerator.generateWordMatch());
 
-            Collections.shuffle(orderList);
+   }
 
-            RightWord1.setText(questions.get(orderList.get(0)).getWelsh());
-            RightWord1.setText(questions.get(orderList.get(1)).getWelsh());
-            RightWord1.setText(questions.get(orderList.get(2)).getWelsh());
-            RightWord1.setText(questions.get(orderList.get(3)).getWelsh());
+   /**
+    * Set chosen words from dictionary on the scene.
+    *
+    * @param questions list of dictionary entries chosen for this test.
+    * @param orderList list of integers to change way of displaying welsh words.
+    */
 
-        }else {
-            LeftWord1.setText(questions.get(0).getWelsh());
-            LeftWord2.setText(questions.get(1).getWelsh());
-            LeftWord3.setText(questions.get(2).getWelsh());
-            LeftWord4.setText(questions.get(3).getWelsh());
 
-            Collections.shuffle(orderList);
+   private void setWords(ArrayList<DictionaryEntry> questions, ArrayList<Integer> orderList){
 
-            RightWord1.setText(questions.get(orderList.get(0)).getEnglish());
-            RightWord1.setText(questions.get(orderList.get(1)).getEnglish());
-            RightWord1.setText(questions.get(orderList.get(2)).getEnglish());
-            RightWord1.setText(questions.get(orderList.get(3)).getEnglish());
-        }
+      if(isEnglish){
+         LeftWord1.setText(questions.get(0).getEnglish());
+         LeftWord2.setText(questions.get(1).getEnglish());
+         LeftWord3.setText(questions.get(2).getEnglish());
+         LeftWord4.setText(questions.get(3).getEnglish());
 
-    }
+         Collections.shuffle(orderList);
 
-    /**
-     * Check if answers from users are correct.
-     */
+         RightWord1.setText(questions.get(orderList.get(0)).getWelsh());
+         RightWord1.setText(questions.get(orderList.get(1)).getWelsh());
+         RightWord1.setText(questions.get(orderList.get(2)).getWelsh());
+         RightWord1.setText(questions.get(orderList.get(3)).getWelsh());
 
-    public void checkAnswers(){
-        ArrayList<String> listOfAnswers = new ArrayList<>();
-        if(isEnglish){
-            listOfAnswers.add(RightWord1.getText());
-            listOfAnswers.add(RightWord2.getText());
-            listOfAnswers.add(RightWord3.getText());
-            listOfAnswers.add(RightWord4.getText());
-        }else {
-            listOfAnswers.add(LeftWord1.getText());
-            listOfAnswers.add(LeftWord2.getText());
-            listOfAnswers.add(LeftWord3.getText());
-            listOfAnswers.add(LeftWord4.getText());
-        }
+      }else {
+         LeftWord1.setText(questions.get(0).getWelsh());
+         LeftWord2.setText(questions.get(1).getWelsh());
+         LeftWord3.setText(questions.get(2).getWelsh());
+         LeftWord4.setText(questions.get(3).getWelsh());
 
-        checkAnswer(setOfQuestions,listOfAnswers,isEnglish);
+         Collections.shuffle(orderList);
 
-        setOfQuestions.clear();
-        this.prepare();
+         RightWord1.setText(questions.get(orderList.get(0)).getEnglish());
+         RightWord1.setText(questions.get(orderList.get(1)).getEnglish());
+         RightWord1.setText(questions.get(orderList.get(2)).getEnglish());
+         RightWord1.setText(questions.get(orderList.get(3)).getEnglish());
+      }
 
-    }
+   }
 
-    /**
-     * Method responsible for preparing questions and scene.
-     */
-    private void prepare(){
-        getQuestions();
-        setWords(setOfQuestions,orderList);
-    }
+   /**
+    * Check if answers from users are correct.
+    */
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+   public void checkAnswers(){
+      ArrayList<String> listOfAnswers = new ArrayList<>();
+      if(isEnglish){
+         listOfAnswers.add(RightWord1.getText());
+         listOfAnswers.add(RightWord2.getText());
+         listOfAnswers.add(RightWord3.getText());
+         listOfAnswers.add(RightWord4.getText());
+      }else {
+         listOfAnswers.add(LeftWord1.getText());
+         listOfAnswers.add(LeftWord2.getText());
+         listOfAnswers.add(LeftWord3.getText());
+         listOfAnswers.add(LeftWord4.getText());
+      }
 
-        this.prepare();
+      checkAnswer(setOfQuestions,listOfAnswers,isEnglish);
 
-    }
+      CorrectAnswer.setText(Integer.toString(correctAnswers));
+      WrongAnswer.setText(Integer.toString(wrongAnswers));
+
+      setOfQuestions.clear();
+      this.prepare();
+
+   }
+
+   /**
+    * Method responsible for preparing questions and scene.
+    */
+   private void prepare(){
+      getQuestions();
+      setWords(setOfQuestions,orderList);
+   }
+
+   @Override
+   public void initialize(URL url, ResourceBundle resourceBundle) {
+
+      this.prepare();
+
+   }
 }
