bb41340481649956f19a87fcf975db02cc316404
Brad Corbett <brc9@aber.ac.uk>
2020-04-28 13:59:38 +0100

    Uploaded currently unfinished Assessment Generator class for others to look at.

--- src/Welsh Vocabulary Tutor/src/main/java/uk/ac/aber/cs22120/group20/selfassessment/AssessmentGenerator.java
+++ src/Welsh Vocabulary Tutor/src/main/java/uk/ac/aber/cs22120/group20/selfassessment/AssessmentGenerator.java
@@ -13,85 +13,100 @@ import java.util.Random;
  * @See
  */
 public class AssessmentGenerator extends Question {
+   /**
+    * Method that will generate a randomized list of questions consisting of random distribution of questions
+    * types, using the dictionary’s practice words as the parameter.
+    * @param wordList
+    * @return
+    */
+   public LinkedList<Question> generateAssessment(LinkedList<DictionaryEntry> wordList){
+      LinkedList<Question> returnValue = new LinkedList<>();
+      Random rand = new Random();
 
-    /**
-     * Method that will generate a randomized list of questions consisting of random distribution of questions
-     * types, using the dictionary’s practice words as the parameter.
-     * @param wordList
-     * @return
-     */
-    public LinkedList<Question> generateAssessment(LinkedList<DictionaryEntry> wordList){
-        return null;
-    }
+      int quizType = rand.nextInt(3);
+      switch(quizType){
+         case(0): //0 Means translation test.
+         int wordToTranslatePlace = rand.nextInt(Application.practiseList.size());
+         DictionaryEntry wordToTranslate = Application.practiseList.get(wordToTranslatePlace);
 
-    /**
-     * Method
-     * that will generate a list of questions that are the type ‘Match The Meanings’, using the dictionary's
-     * practice words as the parameter.
-     * @return
-     */
-    public LinkedList<Question> generateWordMatch(LinkedList<DictionaryEntry> a){
-        return null;
-
-    }
-
-    /**
-     * Method
-     * that will generate a list of questions that are the type ‘6 Meanings’, using the dictionary's practice
-     * words as the parameter.
-     * @return
-     */
-    public static void generateSixMeanings(LinkedList<DictionaryEntry> practiseList){
-
-        //CHANGE DICTIONARY TO PRACTISE LIST
-
-        Random rand = new Random();
+         case(1): //1 Means six meanings test.
 
 
-        boolean isDuplicate = false;
+         case(2): //2 Means match meanings test.
 
-        do{
-            int rand_q=rand.nextInt(Application.dictionary.size()-1);
 
-            DictionaryEntry pickedQuestion = Application.dictionary.get(rand_q);
+      }
 
-            //If size of list is greater than 1 check for duplicates...
-            if(MatchTheMeaningQuestion.setOfQuestions.size()>=1){
+   }
 
-                for (DictionaryEntry setOfQuestion : MatchTheMeaningQuestion.setOfQuestions) {
+   /**
+    * Method
+    * that will generate a list of questions that are the type ‘Match The Meanings’, using the dictionary's
+    * practice words as the parameter.
+    * @return
+    */
+   public LinkedList<Question> generateWordMatch(LinkedList<DictionaryEntry> a){
+      return null;
 
-                    //If it is duplicate change isDuplicate to true and break
-                    if (setOfQuestion.equals(pickedQuestion)) {
-                        isDuplicate = true;
-                        break;
-                    }
+   }
 
-                }
+   /**
+    * Method
+    * that will generate a list of questions that are the type ‘6 Meanings’, using the dictionary's practice
+    * words as the parameter.
+    * @return
+    */
+   public static void generateSixMeanings(LinkedList<DictionaryEntry> practiseList){
 
-                //If duplicate wasn't found add entry to the list
-                if(!isDuplicate){
-                    MatchTheMeaningQuestion.setOfQuestions.add(pickedQuestion);
-                }
+      //CHANGE DICTIONARY TO PRACTISE LIST
 
-                //... otherwise, add entry to the
-            }else{
-                MatchTheMeaningQuestion.setOfQuestions.add(pickedQuestion);
+      Random rand = new Random();
+
+
+      boolean isDuplicate = false;
+
+      do{
+         int rand_q=rand.nextInt(Application.dictionary.size()-1);
+
+         DictionaryEntry pickedQuestion = Application.dictionary.get(rand_q);
+
+         //If size of list is greater than 1 check for duplicates...
+         if(MatchTheMeaningQuestion.setOfQuestions.size()>=1){
+
+            for (DictionaryEntry setOfQuestion : MatchTheMeaningQuestion.setOfQuestions) {
+
+               //If it is duplicate change isDuplicate to true and break
+               if (setOfQuestion.equals(pickedQuestion)) {
+                  isDuplicate = true;
+                  break;
+               }
+
             }
 
-            isDuplicate =false;
+            //If duplicate wasn't found add entry to the list
+            if(!isDuplicate){
+               MatchTheMeaningQuestion.setOfQuestions.add(pickedQuestion);
+            }
 
-        }while(MatchTheMeaningQuestion.setOfQuestions.size()<5);
-    }
+            //... otherwise, add entry to the
+         }else{
+            MatchTheMeaningQuestion.setOfQuestions.add(pickedQuestion);
+         }
 
-    /**
-     * Method that
-     * will generate a list of questions that are the type ‘Translation’, using the dictionary's practice words as
-     * the parameter.
-     * @return
-     */
-    public LinkedList<Question> generateWordEnter(LinkedList<DictionaryEntry> a){
-        return null;
-    }
+         isDuplicate =false;
+
+      }while(MatchTheMeaningQuestion.setOfQuestions.size()<5);
+   }
+
+   /**
+    * Method that
+    * will generate a list of questions that are the type ‘Translation’, using the dictionary's practice words as
+    * the parameter.
+    * @return
+    */
+   public LinkedList<Question> generateWordEnter(LinkedList<DictionaryEntry> a){
+      return null;
+   }
 
 
 }
