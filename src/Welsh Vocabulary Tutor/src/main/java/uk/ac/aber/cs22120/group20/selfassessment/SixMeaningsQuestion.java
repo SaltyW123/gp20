16dc887c3e827d32b72e96620054aa00462a3175
ncw <ncw@aber.ac.uk>
2020-04-28 05:48:02 +0100

    Addition on SixMeaningsQuestion.class

---
+++ src/Welsh Vocabulary Tutor/src/main/java/uk/ac/aber/cs22120/group20/selfassessment/SixMeaningsQuestion.java
@@ -0,0 +1,202 @@
+package uk.ac.aber.cs22120.group20.selfassessment;
+/**
+ * @(#) SixMeaningsQuestion.java 0,1 2020/04/27
+ * <p>
+ * Copyright (c) 2020 Aberystwyth University.
+ * All rights reserved.
+ */
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import uk.ac.aber.cs22120.group20.json.DictionaryEntry;
+
+import java.net.URL;
+import java.util.*;
+
+/**
+ * A class that generate questions and check answers for match the meaning test.
+ *
+ * @author Brad Corbett [brc9]
+ * @author Henry Dugmore [hjd3]
+ * @author Kain Bryan-Jones [kab74]
+ * @author Luke Wybar [law39]
+ * @author Marcin Jakobik [maj83]
+ * @author Oscar Pocock [osp1]
+ * @author Tom Perry [top1]
+ * @author Waylen Watts [ncw]
+ * @version 0.1 Initial development
+ * @see uk.ac.aber.cs22120.group20.javafx.Application
+ */
+
+public class SixMeaningsQuestion extends TranslationController implements Initializable{
+
+    private Random rand = new Random();
+    private LinkedList<DictionaryEntry> wordSet = new LinkedList<>();
+    private ArrayList<Integer> orderList = new ArrayList<>(Arrays.asList(0,1,2,3,4,5));
+    private int correct = 0;
+    private int incorrect = 0;
+    private String englishCounterpart;
+
+
+    @FXML
+    private Label EngWord1;
+
+    @FXML
+    private Label EngWord2;
+
+    @FXML
+    private Label EngWord3;
+
+    @FXML
+    private Label EngWord4;
+
+    @FXML
+    private Label EngWord5;
+
+    @FXML
+    private Label EngWord6;
+
+
+    @FXML
+    private Label WelshWord1;
+
+    @FXML
+    private Label CorrectAnswer;
+
+    @FXML
+    private Label WrongAnswer;
+
+
+/*
+    @FXML
+    private Label WelshWord2;
+
+    @FXML
+    private Label WelshWord3;
+
+    @FXML
+    private Label WelshWord4;
+
+    @FXML
+    private Label WelshWord5;
+
+    @FXML
+    private Label WelshWord6;
+ */
+
+
+    private void getWords(LinkedList<DictionaryEntry> practiceList){
+        boolean isDuplicate = false;
+        do{
+            int word = rand.nextInt(practiceList.size()-1);
+            DictionaryEntry chosenWord = practiceList.get(word);
+
+            if(wordSet.size()>=1){
+
+                for (DictionaryEntry setOfQuestion : wordSet) {
+
+                    if (setOfQuestion.equals(chosenWord)) {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+
+
+                //If duplicate wasn't found add entry to the list
+                if(!isDuplicate){
+                    wordSet.add(chosenWord);
+                }
+
+                //... otherwise, add entry to the
+            }else{
+                wordSet.add(chosenWord);
+            }
+
+            isDuplicate =false;
+
+        }while(wordSet.size()<6);
+
+    }
+
+
+        private void setWords (LinkedList<DictionaryEntry> questions, ArrayList<Integer> orderList){
+        //WelshWord1 Is the question word and as a result is always right.
+            WelshWord1.setText(questions.get(0).getWelsh());
+        //This stores the correct answer for the english word.
+            englishCounterpart = questions.get(0).getEnglish();
+
+
+
+            EngWord1.setText(questions.get(orderList.get(0)).getEnglish());
+            EngWord2.setText(questions.get(orderList.get(1)).getEnglish());
+            EngWord3.setText(questions.get(orderList.get(2)).getEnglish());
+            EngWord4.setText(questions.get(orderList.get(3)).getEnglish());
+            EngWord5.setText(questions.get(orderList.get(4)).getEnglish());
+            EngWord6.setText(questions.get(orderList.get(5)).getEnglish());
+
+            Collections.shuffle(orderList); //I know that this does not belong here it was moved here for debug purposes. It lives five lines up.
+
+        }
+
+
+    public void checkAnswers(){
+        String option1 = EngWord1.toString();
+        String option2 = EngWord2.toString();
+        String option3 = EngWord3.toString();
+        String option4 = EngWord4.toString();
+        String option5 = EngWord5.toString();
+        String option6 = EngWord6.toString();
+
+        if (option1 == englishCounterpart){
+            correct++;
+        }else incorrect++;
+
+        if (option2 == englishCounterpart){
+            correct++;
+        }else incorrect++;
+
+        if (option3 == englishCounterpart){
+            correct++;
+        }else incorrect++;
+
+        if (option4 == englishCounterpart){
+            correct++;
+        }else incorrect++;
+
+        if (option5 == englishCounterpart){
+            correct++;
+        }else incorrect++;
+
+        if (option6 == englishCounterpart){
+            correct++;
+        }else incorrect++;
+
+        CorrectAnswer.setText(Integer.toString(correct));
+
+        WrongAnswer.setText(Integer.toString(incorrect));
+
+        wordSet.clear();
+        this.prepare();
+
+    }
+
+
+
+
+
+
+private void prepare(){
+   // getWords(LinkedList<DictionaryEntry> practiceList);//Thats just out right not correct.... Ask Marcin what he is aiming for to continue.
+        setWords(wordSet, orderList);
+
+}
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.prepare();
+
+    }
+}
