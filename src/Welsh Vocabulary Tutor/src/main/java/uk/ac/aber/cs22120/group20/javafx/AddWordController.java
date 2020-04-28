7564bf16492e0760b52837956484a09dea6594c1
Henry Dugmore <hjd3@aber.ac.uk>
2020-04-28 15:38:02 +0100

    Removed prints used for testing and changed addword.fxml to remove version error
    Edited the Saved success pop up to show the entry that has been added to the dictionary. Also removed the print statements that were used for initial testing and also changed the version in addword.fxml to remove the version error that came up every time.

--- src/Welsh Vocabulary Tutor/src/main/java/uk/ac/aber/cs22120/group20/javafx/AddWordController.java
+++ src/Welsh Vocabulary Tutor/src/main/java/uk/ac/aber/cs22120/group20/javafx/AddWordController.java
@@ -17,9 +17,12 @@ import uk.ac.aber.cs22120.group20.json.DictionaryEntry;
 
 public class AddWordController {
 
-    @FXML private TextField welsh;
-    @FXML private TextField english;
-    @FXML private ComboBox<String> wordType;
+    @FXML
+    private TextField welsh;
+    @FXML
+    private TextField english;
+    @FXML
+    private ComboBox<String> wordType;
 
     public TextField getWelsh() {
         return welsh;
@@ -30,32 +33,30 @@ public class AddWordController {
     }
 
     @FXML
-    private void initialize(){
+    private void initialize() {
 
         wordType.getItems().addAll("Masculine noun", "Feminine noun", "Verb", "Other");
-        System.out.println("Test");
         wordType.setValue("Type");
 
     }
 
 
-    @FXML protected void addButtonClick (ActionEvent actionEvent){
+    @FXML
+    protected void addButtonClick(ActionEvent actionEvent) {
         boolean entryFound = false;
         // one or more blank fields
-        if (english.getText() == null || welsh.getText() == null|| wordType.getValue().equals("Type")){
+        if (english.getText() == null || welsh.getText() == null || wordType.getValue().equals("Type")) {
             Alert error = new Alert(Alert.AlertType.ERROR);
             error.setTitle("Error");
             error.setHeaderText("Entry Not Saved");
             error.setContentText("One or more fields are blank");
 
             error.showAndWait();
-        }
-
-        else {
-            for (DictionaryEntry entry : Application.dictionary){
+        } else {
+            for (DictionaryEntry entry : Application.dictionary) {
                 entryFound = false;
-                DictionaryEntry newEntry = new DictionaryEntry(english.getText(),welsh.getText(),wordType.getValue());
-                if(entry.equals(newEntry)){
+                DictionaryEntry newEntry = new DictionaryEntry(english.getText(), welsh.getText(), wordType.getValue());
+                if (entry.equals(newEntry)) {
                     Alert alert = new Alert(Alert.AlertType.ERROR);
                     alert.setTitle("Error");
                     alert.setHeaderText("Entry Not Saved");
@@ -64,16 +65,15 @@ public class AddWordController {
                     alert.showAndWait();
                     entryFound = true;
                     break;
-                }
-                else{
+                } else {
                     continue;
                 }
             }
-            if(!entryFound){
+            if (!entryFound) {
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
                 alert.setTitle("Success");
                 alert.setHeaderText("Entry Saved");
-                alert.setContentText("Entry Added");
+                alert.setContentText("Entry Added - English: " + english.getText() + " Welsh: " + welsh.getText() + " Type: " + wordType.getValue());
 
                 alert.showAndWait();
                 DictionaryEntry dictionaryEntry = new DictionaryEntry(english.getText(), welsh.getText(), wordType.getValue());
@@ -81,9 +81,9 @@ public class AddWordController {
                 Application.dictionary.add(dictionaryEntry);
 
 //              output of what was saved for testing
-                System.out.print(english.getText());
-                System.out.print(welsh.getText());
-                System.out.println(wordType.getValue());
+//                System.out.print(english.getText());
+//                System.out.print(welsh.getText());
+//                System.out.println(wordType.getValue());
 
 //              Resets values to blank for next word to be entered
                 english.clear();
@@ -108,14 +108,38 @@ public class AddWordController {
     private void switchToPrimary() throws IOException {
         Application.setRoot("Primary");
     }
-// add character methods for characters ch, dd, ff, ng, ll, ph, rh, th
-    public void addCharch(ActionEvent actionEvent) { welsh.appendText("ch"); }
-    public void addChardd(ActionEvent actionEvent) { welsh.appendText("dd"); }
-    public void addCharff(ActionEvent actionEvent) { welsh.appendText("ff"); }
-    public void addCharng(ActionEvent actionEvent) { welsh.appendText("ng"); }
-    public void addCharll(ActionEvent actionEvent) { welsh.appendText("ll"); }
-    public void addCharph(ActionEvent actionEvent) { welsh.appendText("ph"); }
-    public void addCharrh(ActionEvent actionEvent) { welsh.appendText("rh"); }
-    public void addCharth(ActionEvent actionEvent) { welsh.appendText("th"); }
+
+    // add character methods for characters ch, dd, ff, ng, ll, ph, rh, th
+    public void addCharch(ActionEvent actionEvent) {
+        welsh.appendText("ch");
+    }
+
+    public void addChardd(ActionEvent actionEvent) {
+        welsh.appendText("dd");
+    }
+
+    public void addCharff(ActionEvent actionEvent) {
+        welsh.appendText("ff");
+    }
+
+    public void addCharng(ActionEvent actionEvent) {
+        welsh.appendText("ng");
+    }
+
+    public void addCharll(ActionEvent actionEvent) {
+        welsh.appendText("ll");
+    }
+
+    public void addCharph(ActionEvent actionEvent) {
+        welsh.appendText("ph");
+    }
+
+    public void addCharrh(ActionEvent actionEvent) {
+        welsh.appendText("rh");
+    }
+
+    public void addCharth(ActionEvent actionEvent) {
+        welsh.appendText("th");
+    }
 
 }
