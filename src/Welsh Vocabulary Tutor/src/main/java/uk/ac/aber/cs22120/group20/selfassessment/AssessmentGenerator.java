ddedd57988aa04ccc8dd238e7f20729aa10c4a75
law39 <law39@aber.ac.uk>
2020-04-29 17:51:57 +0100

    Reduced code duplication
    Reduced code duplication in Assessment Generator

--- src/Welsh Vocabulary Tutor/src/main/java/uk/ac/aber/cs22120/group20/selfassessment/AssessmentGenerator.java
+++ src/Welsh Vocabulary Tutor/src/main/java/uk/ac/aber/cs22120/group20/selfassessment/AssessmentGenerator.java
@@ -182,16 +182,12 @@ public class AssessmentGenerator {
 
             Optional<ButtonType> result = alert.showAndWait();
 
-
+            currentAssessment = 0;
+            Question.resetScore();
 
             if (result.isEmpty() || result.get() == noBtn) {
-               currentAssessment=0;
-               Question.resetScore();
                ScreenSwitch.swap(ScreenSwitch.SceneEnum.dictionaryScene);
-
             } else {
-               currentAssessment = 0;
-               Question.resetScore();
                 generateAssessment(Application.practiseList);
             }
         }
