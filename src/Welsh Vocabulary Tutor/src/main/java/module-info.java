fab019a69d15769262123d126f9b502b54c923f9
osp1 <osp1@aber.ac.uk>
2020-04-29 09:54:05 +0100

    Added Junit library to project

--- src/Welsh Vocabulary Tutor/src/main/java/module-info.java
+++ src/Welsh Vocabulary Tutor/src/main/java/module-info.java
@@ -1,14 +1,15 @@
 module uk.ac.aber.cs22120.group20 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.fasterxml.jackson.core;
-    requires com.fasterxml.jackson.databind;
+   requires javafx.controls;
+   requires javafx.fxml;
+   requires com.fasterxml.jackson.core;
+   requires com.fasterxml.jackson.databind;
+   requires junit;
 
 
-    opens uk.ac.aber.cs22120.group20.javafx to javafx.fxml;
-    opens uk.ac.aber.cs22120.group20 to javafx.fxml;
-    opens uk.ac.aber.cs22120.group20.json to com.fasterxml.jackson.databind;
+   opens uk.ac.aber.cs22120.group20.javafx to javafx.fxml;
+   opens uk.ac.aber.cs22120.group20 to javafx.fxml;
+   opens uk.ac.aber.cs22120.group20.json to com.fasterxml.jackson.databind;
 
-    exports uk.ac.aber.cs22120.group20.json to com.fasterxml.jackson.databind;
-    exports uk.ac.aber.cs22120.group20.javafx to javafx.graphics, javafx.fxml;
+   exports uk.ac.aber.cs22120.group20.json to com.fasterxml.jackson.databind;
+   exports uk.ac.aber.cs22120.group20.javafx to javafx.graphics, javafx.fxml;
 }
