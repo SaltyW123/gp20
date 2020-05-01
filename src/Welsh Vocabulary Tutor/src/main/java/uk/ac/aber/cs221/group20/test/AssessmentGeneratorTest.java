0cdd05969875ff3b90531675fbc12b0fe352c12e
osp1 <osp1@aber.ac.uk>
2020-05-01 15:44:15 +0100

    JavaDoc for AssessmentGenerator
    - Issue #42

--- src/Welsh Vocabulary Tutor/src/main/java/uk/ac/aber/cs221/group20/test/AssessmentGeneratorTest.java
+++ src/Welsh Vocabulary Tutor/src/main/java/uk/ac/aber/cs221/group20/test/AssessmentGeneratorTest.java
@@ -1,26 +1,42 @@
+/*
+ * @(#) AssessmentGeneratorTest.java 0,1 2020/05/01
+ * <p>
+ * Copyright (c) 2020 Aberystwyth University.
+ * All rights reserved.
+ */
 package uk.ac.aber.cs221.group20.test;
 
 import org.junit.jupiter.api.Test;
 import uk.ac.aber.cs221.group20.selfassessment.AssessmentGenerator;
 import uk.ac.aber.cs221.group20.selfassessment.Question;
 import uk.ac.aber.cs221.group20.javafx.Application;
-import uk.ac.aber.cs221.group20.selfassessment.AssessmentGenerator;
-import uk.ac.aber.cs221.group20.selfassessment.Question;
 
 import java.util.LinkedList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * A test class for various tests regarding AssessmentGenerator.
+ * @author Luke Wybar [law]
+ */
 public class AssessmentGeneratorTest extends AssessmentGenerator {
 
-    @Test
-    void testNumOfAssessments() {
-        LinkedList<Question> listOfAssessments;
-        listOfAssessments = generateAssessment(Application.dictionary);
 
-        assertEquals(10,listOfAssessments.size());
+   // ////////////// //
+   // Class methods. //
+   // ////////////// //
 
-    }
+   /**
+    * Tests to see if the right amount of tests is generated.
+    */
+   @Test
+   void testNumOfAssessments() {
+      LinkedList<Question> listOfAssessments;
+      listOfAssessments = generateAssessment(Application.dictionary);
+
+      assertEquals(10,listOfAssessments.size());
+
+   }
 
 
 
